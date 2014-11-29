3123691c56bfe167d4ac614c91ca7668be1ef1ee
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-11-29 18:46:59 +0900

    readTdiary return empty Pandoc

--- src/Text/Pandoc/Readers/Tdiary.hs
+++ src/Text/Pandoc/Readers/Tdiary.hs
@@ -1,3 +1,13 @@
-module Text.Pandoc.Readers.Tdiary where
+module Text.Pandoc.Readers.Tdiary ( readTdiary
+                                  ) where
 
+import Text.Pandoc.Definition
+import Text.Pandoc.Options (ReaderOptions(readerParseRaw, readerTrace))
 import Text.Pandoc.Readers.HTML
+import Text.Parsec
+
+-- | Convert tDiary-formatted string to 'Pandoc' document.
+readTdiary :: ReaderOptions -- ^ Reader options
+           -> String        -- ^ String to parse
+           -> Pandoc
+readTdiary opts inp = Pandoc nullMeta []
